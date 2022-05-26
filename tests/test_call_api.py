407b8e291de03b20b15accdbe2ed29ad00e8d718
yanyongyu <42488585+yanyongyu@users.noreply.github.com>
2022-05-26 16:49:09 +0800

    :white_check_mark: fix store test

--- tests/test_call_api.py
+++ tests/test_call_api.py
@@ -7,25 +7,20 @@ from nonebug import App
 @pytest.mark.asyncio
 async def test_api_reply(app: App, init_adapter):
     from nonebot.adapters.onebot.v11 import Adapter
-    from nonebot.adapters.onebot.store import ResultStore
-
-    result_store = ResultStore()
 
     self_id = "test"
-    seq = result_store.get_seq()
+    seq = Adapter._result_store.get_seq()
     data = {"test": "test"}
     response_data = {
         "status": "success",
         "retcode": 0,
         "data": data,
-        "echo": {
-            "seq": seq,
-        },
+        "echo": str(seq),
     }
 
     async def feed_result():
         Adapter.json_to_event(response_data, self_id)
 
     asyncio.create_task(feed_result())
-    resp = await result_store.fetch(self_id, seq, 10.0)
+    resp = await Adapter._result_store.fetch(self_id, seq, 10.0)
     assert resp == response_data
