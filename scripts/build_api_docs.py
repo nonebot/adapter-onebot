d6e0140dd0be1ae3e358fa04274181ba85c43bb6
hemengyang <hmy0119@gmail.com>
2023-02-28 11:49:18 +0800

    :bug: 修复 nb_autodoc 并不再使用 git 版本

--- scripts/build_api_docs.py
+++ scripts/build_api_docs.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from nb_autodoc import Module
+from nb_autodoc import ModuleManager
+from nb_autodoc.config import Config
 from nb_autodoc.builders.markdown import MarkdownBuilder
 
 import nonebot.adapters
@@ -9,8 +10,7 @@ nonebot.adapters.__path__.append(  # type: ignore
     str((Path(__file__).parent.parent / "nonebot" / "adapters").resolve())
 )
 
-module = Module("nonebot.adapters.onebot")
-builder = MarkdownBuilder(
-    module, output_dir=str((Path(__file__).parent.parent / "build").resolve())
-)
+config = Config(output_dir=str((Path(__file__).parent.parent / "build").resolve()))
+module = ModuleManager("nonebot.adapters.onebot", config=config)
+builder = MarkdownBuilder(module)
 builder.write()
