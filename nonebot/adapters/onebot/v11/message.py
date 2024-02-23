430aba012a6603c0e69906e0e2ba3388098ccc37
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-02-23 15:38:29 +0800

    :loud_sound: remove forward warning

--- nonebot/adapters/onebot/v11/message.py
+++ nonebot/adapters/onebot/v11/message.py
@@ -18,7 +18,7 @@ from nonebot.adapters.onebot.utils import rich_escape
 from nonebot.adapters.onebot.utils import truncate as trunc
 from nonebot.adapters import MessageSegment as BaseMessageSegment
 
-from .utils import log, escape, unescape
+from .utils import escape, unescape
 
 
 class MessageSegment(BaseMessageSegment["Message"]):
@@ -102,7 +102,6 @@ class MessageSegment(BaseMessageSegment["Message"]):
 
     @classmethod
     def forward(cls, id_: str) -> Self:
-        log("WARNING", "Forward Message only can be received!")
         return cls("forward", {"id": id_})
 
     @classmethod
