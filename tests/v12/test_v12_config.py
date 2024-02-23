85e409c03bb71ac0bde967697d7796863c39ed2a
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-02-23 11:16:42 +0800

    :white_check_mark: fix test config

--- tests/v12/test_v12_config.py
+++ tests/v12/test_v12_config.py
@@ -6,4 +6,4 @@ def test_config():
     adapter = get_adapter(Adapter)
     config = adapter.onebot_config
 
-    assert config.onebot_access_token == "test"
+    assert config.onebot_access_token == "test2"
